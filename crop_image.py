5b38f9eb0c431c4467c1ae411bff6b082391f582
Quick <quick0831@gmail.com>
2021-09-29 02:36:19 +0800

    Use Sprite To Create A Board

--- crop_image.py
+++ crop_image.py
@@ -5,8 +5,33 @@ CHESS_PATH = os.path.join(".","chess_piece_200x200.png")
 
 img = pygame.image.load(CHESS_PATH)
 
+class Piece():
+    KING = 0
+    QUEEN = 1
+    BISHOP = 2
+    KNIGHT = 3
+    ROOK = 4
+    PAWN = 5
+    EMPTY = 6
+
+class Color():
+    WHITE = 0
+    BLACK = 1
+
+class State:
+    def __init__(self, piece, color):
+        self.piece = piece
+        self.color = color
+
 def main(piece_size):
     piece_size = int(piece_size)
-    pieces = [[pygame.transform.smoothscale(img.subsurface((j*200, i*200, 200, 200)), (piece_size, piece_size)) for j in range(6)] for i in range(2)]
+    pieces = [[0]*7]*2
+    for i in range(2):
+        for j in range(6):
+            pieces[i][j] = pygame.transform.smoothscale(img.subsurface((j*200, i*200, 200, 200)), (piece_size, piece_size))
+        
+        s = pygame.Surface((piece_size, piece_size))
+        s.set_colorkey((0,0,0))
+        pieces[i][Piece.EMPTY] = s
     
     return pieces
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 from pygame.locals import QUIT, VIDEORESIZE
 
 import crop_image
+from crop_image import Piece, Color, State
 
 size = (820, 820)
 title = "Chess"
@@ -24,6 +25,32 @@ pygame.display.set_caption(title)
 
 clk = pygame.time.Clock()
 
+class Space(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.x = x
+        self.y = y
+        self.resize()
+        self.update()
+    
+    def resize(self):
+        self.piece_size = piece_size
+        self.pieces = pieces
+
+    def update(self):
+        s = board_state[self.x][self.y]
+        self.image = pieces[s.color][s.piece]
+        self.rect = (self.x*piece_size + board_offset[0], self.y*piece_size + board_offset[1])
+
+board_state = [[State(Piece.EMPTY, Color.WHITE) for _ in range(8)] for _ in range(8)]
+
+board_state[2][3] = State(Piece.BISHOP, Color.BLACK)
+
+Board = pygame.sprite.Group()
+for i in range(8):
+    for j in range(8):
+        Board.add(Space(i, j))
+
 while True:
     # ====  Event Loop ====
     for event in pygame.event.get():
@@ -36,19 +63,18 @@ while True:
             piece_size = (min(w,h)-20)/8
             board_offset
             pieces = crop_image.main(piece_size)
+            for space in Board:
+                space.resize(piece_size, pieces)
     # ==== End of Event Loop ====
     screen.fill(BLACK)
     for i in range(8):
         for j in range(8):
             pygame.draw.rect(screen, DARK if (i+j)%2==1 else BRIGHT, (i*piece_size + board_offset[0], j*piece_size + board_offset[1], piece_size+1, piece_size+1))
-    for i in range(2):
-        for j in range(6):
-            screen.blit(pieces[i][j], (j*piece_size + board_offset[0],  i   *piece_size + board_offset[1]))
-            screen.blit(pieces[i][j], (j*piece_size + board_offset[0], (i+3)*piece_size + board_offset[1]))
-    
-    pygame.draw.rect(screen, DARK_RED, (3*piece_size + board_offset[0], 6*piece_size + board_offset[1], piece_size+1, piece_size+1))
-    screen.blit(pieces[0][0], (3*piece_size + board_offset[0], 6*piece_size + board_offset[1]))
-    pygame.draw.rect(screen, BRIGHT_RED, (4*piece_size + board_offset[0], 6*piece_size + board_offset[1], piece_size+1, piece_size+1))
-    screen.blit(pieces[1][0], (4*piece_size + board_offset[0], 6*piece_size + board_offset[1]))
+    Board.draw(screen)
+
+    #pygame.draw.rect(screen, DARK_RED, (3*piece_size + board_offset[0], 6*piece_size + board_offset[1], piece_size+1, piece_size+1))
+    #screen.blit(pieces[0][0], (3*piece_size + board_offset[0], 6*piece_size + board_offset[1]))
+    #pygame.draw.rect(screen, BRIGHT_RED, (4*piece_size + board_offset[0], 6*piece_size + board_offset[1], piece_size+1, piece_size+1))
+    #screen.blit(pieces[1][0], (4*piece_size + board_offset[0], 6*piece_size + board_offset[1]))
     clk.tick(FPS)
     pygame.display.update()
